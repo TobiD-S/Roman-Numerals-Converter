c30cdce59762776ef400cabc6ed88944e34221f0
Tobi <delesafatobi@gmail.com>
2019-10-16 23:05:55 +0100

    Adding event listeners

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
+const arabicInput = document.getElementById("arabicInput");
+const romanInput = document.getElementById("romanInput");
+
+arabicInput.addEventListener("input", (e) => {
+    romanInput.value = arabicToRoman(e.target.value);
+});
+romanInput.addEventListener("input", (e) => {
+    arabicInput.value = romanToArabic(e.target.value);
+
+});
+
 function arabicToRoman(number) {
     let roman = "";
     const romanNumList = { M: 1000, CM: 900, D: 500, CD: 400, C: 100, XC: 90, L: 50, XV: 40, X: 10, IX: 9, V: 5, IV: 4, I: 1 };
